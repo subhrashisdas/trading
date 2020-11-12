330a552f9b475b3f3815c20d048f28286cc8655f
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-12 12:52:58 +0530

    Fixed .gitignore

---
+++ src/candle.ts
@@ -0,0 +1,47 @@
+import { differenceInMinutes, startOfMonth, addMonths, parseISO } from 'date-fns';
+
+export interface Candle {
+  timestamp: Date;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+}
+
+export enum Interval {
+  'minute' = 'minute',
+  '3minute' = '3minute',
+  '5minute' = '5minute',
+  '10minute' = '10minute',
+  '15minute' = '15minute',
+  '30minute' = '30minute',
+  'hour' = 'hour',
+  'day' = 'day',
+  'week' = 'week',
+  'month' = 'month',
+  'year' = 'year',
+}
+
+export function convertInterval(candles: Candle[], interval: Interval) {
+  const newCandles = [];
+  let maxTimeFrame;
+  for (const candle of candles) {
+    const currentProcessedCandle = newCandles.pop();
+    if (maxTimeFrame && differenceInMinutes(maxTimeFrame, candle.timestamp) > 0 && currentProcessedCandle) {
+      const newProcessedCandle: Candle = {
+        timestamp: startOfMonth(candle.timestamp),
+        high: Math.max(currentProcessedCandle.high, candle.high),
+        low: Math.min(currentProcessedCandle.low, candle.low),
+        open: currentProcessedCandle.open,
+        close: candle.close,
+        volume: currentProcessedCandle.volume + candle.volume,
+      };
+      newCandles.push(newProcessedCandle);
+    } else {
+      maxTimeFrame = startOfMonth(addMonths(candle.timestamp, 1));
+      newCandles.push(candle);
+    }
+  }
+  return newCandles;
+}
