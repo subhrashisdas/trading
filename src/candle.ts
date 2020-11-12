cddb1c45bbb5e424121a0224d0b8488df239e2f3
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-13 01:16:09 +0530

    Added function to convertInterval

--- src/candle.ts
+++ src/candle.ts
@@ -1,14 +1,5 @@
-import {
-  differenceInMinutes,
-  startOfMonth,
-  startOfMinute,
-  startOfHour,
-  addMonths,
-  startOfWeek,
-  parseISO,
-  startOfDay,
-  startOfYear,
-} from 'date-fns';
+import { differenceInMinutes } from 'date-fns';
+import { ceilOfInterval, TimeUnit } from '@src/date';
 
 export interface Candle {
   timestamp: Date;
@@ -19,92 +10,27 @@ export interface Candle {
   volume: number;
 }
 
-export enum Interval {
-  'minute' = 'minute',
-  '3minute' = '3minute',
-  '5minute' = '5minute',
-  '10minute' = '10minute',
-  '15minute' = '15minute',
-  '30minute' = '30minute',
-  'hour' = 'hour',
-  'day' = 'day',
-  'week' = 'week',
-  'month' = 'month',
-  'year' = 'year',
-}
-
-export function convertInterval(candles: Candle[], interval: Interval) {
-  const newCandles = [];
+export function convertInterval(candles: Candle[], interval: number, timeUnit: TimeUnit) {
+  const newCandles: Candle[] = [];
   let maxTimeFrame;
   for (const candle of candles) {
     const currentProcessedCandle = newCandles.pop();
     if (maxTimeFrame && differenceInMinutes(maxTimeFrame, candle.timestamp) > 0 && currentProcessedCandle) {
-      const newProcessedCandle: Candle = {
-        ...currentProcessedCandle,
-        high: Math.max(currentProcessedCandle.high, candle.high),
-        low: Math.min(currentProcessedCandle.low, candle.low),
-        open: currentProcessedCandle.open,
-        volume: currentProcessedCandle.volume + candle.volume,
-      };
-      newCandles.push(newProcessedCandle);
+      newCandles.push(mergeCandle(currentProcessedCandle, candle));
     } else {
-      maxTimeFrame = startOfMonth(addMonths(candle.timestamp, 1));
+      maxTimeFrame = ceilOfInterval(candle.timestamp, interval, timeUnit);
       newCandles.push(candle);
     }
   }
   return newCandles;
 }
 
-export function startOfInterval(timestamp: Date, interval: Interval) {
-  switch (interval) {
-    case Interval.minute:
-      return startOfMinute(timestamp);
-    case Interval['3minute']:
-      return startOfMinute(timestamp);
-    case Interval['5minute']:
-      return startOfMinute(timestamp);
-    case Interval['10minute']:
-      return startOfMinute(timestamp);
-    case Interval['15minute']:
-      return startOfMinute(timestamp);
-    case Interval['30minute']:
-      return startOfMinute(timestamp);
-    case Interval.hour:
-      return startOfHour(timestamp);
-    case Interval.day:
-      return startOfDay(timestamp);
-    case Interval.week:
-      return startOfWeek(timestamp);
-    case Interval.month:
-      return startOfMonth(timestamp);
-    case Interval.year:
-      return startOfYear(timestamp);
-  }
-}
-
-export function addInterval(timestamp: Date, interval: Interval, amount: number) {
-  switch (interval) {
-    case Interval.minute:
-      return startOfMinute(timestamp);
-    case Interval['3minute']:
-      return startOfMinute(timestamp);
-    case Interval['5minute']:
-      return startOfMinute(timestamp);
-    case Interval['10minute']:
-      return startOfMinute(timestamp);
-    case Interval['15minute']:
-      return startOfMinute(timestamp);
-    case Interval['30minute']:
-      return startOfMinute(timestamp);
-    case Interval.hour:
-      return startOfMinute(timestamp);
-    case Interval.day:
-      return startOfMinute(timestamp);
-    case Interval.week:
-      return startOfMinute(timestamp);
-    case Interval.month:
-      return startOfMinute(timestamp);
-    case Interval.year:
-      return startOfMinute(timestamp);
-  }
+function mergeCandle(oldCandle: Candle, newCandle: Candle): Candle {
+  return {
+    ...oldCandle,
+    high: Math.max(oldCandle.high, newCandle.high),
+    low: Math.min(oldCandle.low, newCandle.low),
+    open: oldCandle.open,
+    volume: oldCandle.volume + newCandle.volume,
+  };
 }
--- src/date.ts
+++ src/date.ts
@@ -17,3 +17,25 @@ export function ceilToNearestMinutes(date: Date, minutes: number) {
   clonedDate.setMinutes(clonedDateMinutes - remainder + minutes);
   return clonedDate;
 }
+
+export enum TimeUnit {
+  minute = 'minute',
+  hour = 'hour',
+  day = 'day',
+  week = 'week',
+  month = 'month',
+  year = 'year',
+}
+
+export function ceilOfInterval(date: Date, interval: number, timeUnit: TimeUnit) {
+  switch (timeUnit) {
+    case TimeUnit.minute:
+      return ceilToNearestMinutes(date, interval);
+    case TimeUnit.hour:
+      return ceilToNearestMinutes(date, interval * 60);
+    case TimeUnit.day:
+      return ceilToNearestMinutes(date, interval * 60 * 24);
+    case TimeUnit.week:
+      return ceilToNearestMinutes(date, interval * 60 * 24 * 7);
+  }
+}
