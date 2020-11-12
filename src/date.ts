96d16aac7e3837bf96acbae44523cbbba9dcc821
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-13 01:34:48 +0530

    Added Ceil To Nearest Months and years

--- src/date.ts
+++ src/date.ts
@@ -18,6 +18,24 @@ export function ceilToNearestMinutes(date: Date, minutes: number) {
   return clonedDate;
 }
 
+export function ceilToNearestYears(date: Date, years: number) {
+  const clonedDate = new Date(date);
+  const year = clonedDate.getFullYear();
+  const remainder = year % years;
+  clonedDate.setFullYear(year - remainder + years, 0, 0);
+  clonedDate.setHours(0, 0, 0, 0);
+  return clonedDate;
+}
+
+export function ceilToNearestMonths(date: Date, months: number) {
+  const clonedDate = new Date(date);
+  const month = date.getMonth();
+  const remainder = month % months;
+  clonedDate.setFullYear(date.getFullYear(), month - remainder + months, 0);
+  clonedDate.setHours(0, 0, 0, 0);
+  return clonedDate;
+}
+
 export enum TimeUnit {
   minute = 'minute',
   hour = 'hour',
@@ -27,7 +45,7 @@ export enum TimeUnit {
   year = 'year',
 }
 
-export function ceilOfInterval(date: Date, interval: number, timeUnit: TimeUnit) {
+export function ceilOfInterval(date: Date, interval: number, timeUnit: TimeUnit): Date {
   switch (timeUnit) {
     case TimeUnit.minute:
       return ceilToNearestMinutes(date, interval);
@@ -37,5 +55,9 @@ export function ceilOfInterval(date: Date, interval: number, timeUnit: TimeUnit)
       return ceilToNearestMinutes(date, interval * 60 * 24);
     case TimeUnit.week:
       return ceilToNearestMinutes(date, interval * 60 * 24 * 7);
+    case TimeUnit.month:
+      return ceilToNearestMonths(date, interval);
+    case TimeUnit.year:
+      return ceilToNearestYears(date, interval);
   }
 }
