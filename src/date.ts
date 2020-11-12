9d43a8ff8da20f4ff6ac091820c0c5572387cfcb
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-12 21:47:32 +0530

    Added ceilToNearestMinutes function

--- src/date.ts
+++ src/date.ts
@@ -7,3 +7,13 @@ export function floatToNearestMinutes(date: Date, minutes: number) {
   clonedDate.setMinutes(clonedDateMinutes - remainder);
   return clonedDate;
 }
+
+export function ceilToNearestMinutes(date: Date, minutes: number) {
+  const clonedDate = new Date(date);
+  clonedDate.setSeconds(0);
+  clonedDate.setMilliseconds(0);
+  const clonedDateMinutes = clonedDate.getMinutes();
+  const remainder = clonedDateMinutes % minutes;
+  clonedDate.setMinutes(clonedDateMinutes - remainder + minutes);
+  return clonedDate;
+}
