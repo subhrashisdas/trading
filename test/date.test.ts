7d2f7eb844438136be6df6f7929fcbacdc78ac06
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-14 19:27:58 +0530

    Revoked code for usage of epoch time

--- test/date.test.ts
+++ test/date.test.ts
@@ -1,17 +1,4 @@
-import { floatToNearestMinutes, ceilToNearestMinutes } from '@src/date';
 import { isEqual } from 'date-fns';
 import { ok } from 'assert';
 
-export function floatToNearestMinutesTest() {
-  ok(isEqual(floatToNearestMinutes(new Date('2020-11-12T14:11:47.822Z'), 15), new Date('2020-11-12T14:00:00.0Z')));
-  ok(isEqual(floatToNearestMinutes(new Date('2020-11-12T14:11:47.822Z'), 10), new Date('2020-11-12T14:10:00.0Z')));
-  ok(isEqual(floatToNearestMinutes(new Date('2020-11-12T14:11:47.822Z'), 3), new Date('2020-11-12T14:09:00.0Z')));
-  ok(isEqual(floatToNearestMinutes(new Date('2020-11-12T14:27:23+05:15'), 15), new Date('2020-11-12T14:15:00+05:15')));
-}
-
-export function ceilToNearestMinutesTest() {
-  ok(isEqual(ceilToNearestMinutes(new Date('2020-11-12T14:11:47.822Z'), 15), new Date('2020-11-12T14:15:00.0Z')));
-  ok(isEqual(ceilToNearestMinutes(new Date('2020-11-12T14:11:47.822Z'), 10), new Date('2020-11-12T14:20:00.0Z')));
-  ok(isEqual(ceilToNearestMinutes(new Date('2020-11-12T14:11:47.822Z'), 3), new Date('2020-11-12T14:12:00.0Z')));
-  ok(isEqual(ceilToNearestMinutes(new Date('2020-11-12T14:27:23+05:15'), 15), new Date('2020-11-12T14:30:00+05:15')));
-}
+export function ceilToNearestMinutesTest() {}
