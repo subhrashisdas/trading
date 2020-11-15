6a16812921bb1dac13ceae9657ca8ed4b6ac9b05
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-15 21:35:09 +0530

    Added new times

--- src/date.ts
+++ src/date.ts
@@ -1,8 +1,10 @@
 export type EpochTime = number;
 export type Milliseconds = number;
 
-export const MinuteInMs = 60 * 1000;
-export const DayInMs = 24 * 60 * MinuteInMs;
+export const SecondInMs = 1000;
+export const MinuteInMs = 60 * SecondInMs;
+export const HourInMs = 60 * MinuteInMs;
+export const DayInMs = 24 * HourInMs;
 export const WeekInMs = 7 * DayInMs;
 
 export function ceilToNearestMilliseconds(date: EpochTime, milliseconds: Milliseconds) {
