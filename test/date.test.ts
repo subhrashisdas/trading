a3dccace80994cae6d2fdd35a00ab8a17451a6aa
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-15 20:29:28 +0530

    Moved week function

--- test/date.test.ts
+++ test/date.test.ts
@@ -1,9 +1,9 @@
 import { deepStrictEqual } from 'assert';
-import { ceilToNearestMilliseconds } from '@src/date';
+import { ceilToNearestMilliseconds, WeekInMs } from '@src/date';
 
 export function ceilToNearestMillisecondsTest() {
   deepStrictEqual(
-    new Date(ceilToNearestMilliseconds(1605451552423, 604800000)).toISOString(),
+    new Date(ceilToNearestMilliseconds(1605451552423, WeekInMs)).toISOString(),
     new Date('2020-11-16T00:00:00.0Z').toISOString()
   );
 }
