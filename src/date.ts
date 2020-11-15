9576f5114a7d8821a7c6419912f4691901026de0
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-15 20:28:22 +0530

    Added simple epoch function to get ceil data

--- src/date.ts
+++ src/date.ts
@@ -1,3 +1,10 @@
 export type EpochTime = number;
+export type Milliseconds = number;
 
-export function ceilToNearestMinutes(date: EpochTime, minutes: number) {}
+export const MinuteInMs = 60 * 1000;
+export const DayInMs = 24 * 60 * MinuteInMs;
+export const WeekInMs = DayInMs * 7;
+
+export function ceilToNearestMilliseconds(date: EpochTime, milliseconds: Milliseconds) {
+  return date - (date % milliseconds) + milliseconds - 3 * DayInMs;
+}
--- test/date.test.ts
+++ test/date.test.ts
@@ -1,4 +1,9 @@
-import { isEqual } from 'date-fns';
-import { ok } from 'assert';
+import { deepStrictEqual } from 'assert';
+import { ceilToNearestMilliseconds } from '@src/date';
 
-export function ceilToNearestMinutesTest() {}
+export function ceilToNearestMillisecondsTest() {
+  deepStrictEqual(
+    new Date(ceilToNearestMilliseconds(1605451552423, 604800000)).toISOString(),
+    new Date('2020-11-16T00:00:00.0Z').toISOString()
+  );
+}
