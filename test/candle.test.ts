4b32d3f2b05e2cbc55599311e750701375280087
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-15 22:45:53 +0530

    Added candle data

--- test/candle.test.ts
+++ test/candle.test.ts
@@ -1,10 +1,10 @@
-import { Candle, convertInterval } from "@src/candle";
-import { MinuteInMs } from "@src/date"
+import { Candle, convertInterval } from '@src/candle';
+import { MinuteInMs } from '@src/date';
 import { deepStrictEqual } from 'assert';
 
 export function convertIntervalTest() {
-    const oneMinuteCandles: Candle[] = []
-    const fiveMinuteCandles: Candle[] = []
-    convertInterval(oneMinuteCandles, MinuteInMs)
-    deepStrictEqual(oneMinuteCandles, fiveMinuteCandles)
+  const oneMinuteCandles: Candle[] = [];
+  const fiveMinuteCandles: Candle[] = [];
+  convertInterval(oneMinuteCandles, MinuteInMs);
+  deepStrictEqual(oneMinuteCandles, fiveMinuteCandles);
 }
