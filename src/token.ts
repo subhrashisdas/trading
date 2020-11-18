7d96b77842f4517c5125925b852ebd61c65f8b55
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-18 10:15:18 +0530

    Added token logic

---
+++ src/token.ts
@@ -0,0 +1,66 @@
+import { writeFile, readFile } from 'fs/promises';
+import { post } from 'got';
+
+const folderLocation = '.cache/credentials.json';
+const envLocation = '../env.json';
+
+interface Config {
+  userId: string;
+  password: string;
+  pin: string;
+}
+
+interface LoginData {
+  cookies: string;
+  requestId: string;
+}
+
+async function login(config: Config) {
+  const { body, headers } = await post('https://kite.zerodha.com/api/login', {
+    form: {
+      user_id: config.userId,
+      password: config.password,
+    },
+    responseType: 'json',
+  });
+
+  return {
+    requestId: body.data['request_id'],
+    cookies: headers['set-cookie'],
+  };
+}
+
+async function twoFa(config: Config, loginData: LoginData) {
+  const { body, headers } = await post('https://kite.zerodha.com/api/twofa', {
+    headers: {
+      cookie: loginData.cookies,
+    },
+    form: {
+      user_id: config.userId,
+      request_id: loginData.requestId,
+      twofa_value: config.pin,
+    },
+    responseType: 'json',
+  });
+
+  return {
+    authorization: `enctoken ${JSON.stringify(headers).match('enctoken=(.*); path')[1]}`,
+  };
+}
+
+async function generateCredentials(config: Config) {
+  const loginData = await login(config);
+  const twoFaData = await twoFa(config, loginData);
+  return {
+    ...config,
+    ...twoFaData,
+  };
+}
+
+export async function createCredentials() {
+  await writeFile(folderLocation, JSON.stringify(await generateCredentials()));
+}
+
+export async function getCredentials() {
+  return JSON.parse((await readFile(folderLocation)).toString());
+}
