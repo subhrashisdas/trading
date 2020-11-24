1e4d3320e68cbfef5dbb35d2b5d7bff4944f47b3
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-24 13:46:27 +0530

    Added order API

---
+++ src/order.ts
@@ -0,0 +1,32 @@
+export interface Order {
+  placed_by: string;
+  order_id: string;
+  exchange_order_id: null;
+  parent_order_id: null;
+  status: string;
+  status_message: string;
+  status_message_raw: string;
+  order_timestamp: Date;
+  exchange_update_timestamp: null;
+  exchange_timestamp: null;
+  variety: string;
+  exchange: string;
+  tradingsymbol: string;
+  instrument_token: number;
+  order_type: string;
+  transaction_type: string;
+  validity: string;
+  product: string;
+  quantity: number;
+  disclosed_quantity: number;
+  price: number;
+  trigger_price: number;
+  average_price: number;
+  filled_quantity: number;
+  pending_quantity: number;
+  cancelled_quantity: number;
+  market_protection: number;
+  meta: object;
+  tag: null;
+  guid: string;
+}
