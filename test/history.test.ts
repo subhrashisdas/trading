2df388b7791e124f6b20d20bdc134111ee96c564
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-26 10:48:18 +0530

    Revert "Deplricated getOptimizedHistory function"
    This reverts commit ebf5f67c6f5ce1d492a011ba6e946fffe5d608b1.

--- test/history.test.ts
+++ test/history.test.ts
@@ -1,4 +1,4 @@
-import { candlestick, history } from '@src/history';
+import { candlestick, history, getOptimizedHistory } from '@src/history';
 import { ok } from 'assert';
 
 export async function candlestickTest() {
@@ -9,4 +9,14 @@ export async function candlestickTest() {
 export async function normalHistoryTest() {
   const candlestickData = await history(263433, new Date('2020-10-05').getTime(), new Date('2020-10-05').getTime());
   ok(candlestickData.length === 375);
+}
+
+export async function getOptimizedHistoryTest() {
+  const candlestickData = await getOptimizedHistory(
+    263433,
+    new Date('2020-10-05').getTime(),
+    new Date('2020-10-05').getTime()
+  );
+  console.log(JSON.stringify(candlestickData));
+  ok(candlestickData.length > 0);
 }
