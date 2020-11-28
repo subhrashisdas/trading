8f4e868574a764f63d1b1f6eee7a12e7833708df
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-28 20:14:53 +0530

    Added JSON set get delete function.

--- src/fs.ts
+++ src/fs.ts
@@ -1,4 +1,5 @@
-import { access, unlink } from 'fs/promises';
+import { access, unlink, writeFile, readFile } from 'fs/promises';
+import path from 'path';
 
 export async function exists(filePath: string): Promise<Boolean> {
   try {
@@ -15,5 +16,37 @@ export async function deleteFile(filePath: string): Promise<Boolean> {
     return true;
   } catch (_) {
     return false;
+  }
+}
+
+const folderLocation = path.join(__filename, '../../.cache');
+
+export function filePath(key: string) {
+  return path.join(folderLocation, key);
+}
+
+export async function setJson(key: string, value: object) {
+  try {
+    await writeFile(filePath(key), JSON.stringify(value));
+    return value;
+  } catch (_) {
+    return;
+  }
+}
+
+export async function getJson(key: string) {
+  try {
+    return JSON.parse((await readFile(filePath(key))).toString());
+  } catch (_) {
+    return;
+  }
+}
+
+export async function deleteJson(key: string) {
+  try {
+    await unlink(filePath(key));
+    return;
+  } catch (_) {
+    return;
   }
 }
