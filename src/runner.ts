6ff06f50743e008e7e2bb432c1db6162beae184b
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-30 17:30:09 +0530

    Removed un-used function

--- src/runner.ts
+++ src/runner.ts
@@ -1,6 +1,3 @@
 import { Candle } from '@src/candle';
-import { Milliseconds } from '@src/date';
 
-export async function runAlgoOfACandle(candle: Candle, algoName: string, type: 'trade' | 'squareoff') {}
-
-export async function runAlgoForADay(dateTime: Milliseconds, algoName: string) {}
+export async function runAlgo(candles: Candle[], algoName: string) {}
