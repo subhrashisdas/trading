cfc2f7c2161a8413e133d605f73d5ccdd5279c47
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-30 15:30:58 +0530

    Added Runner function

--- src/algo.ts
+++ src/algo.ts
@@ -1,7 +1,9 @@
 import { Candle } from '@src/candle';
-import { WeekInMs } from '@src/date';
+import { MinuteInMs, WeekInMs } from '@src/date';
 
 export const candlesLimit = WeekInMs;
+export const timeInterval = 5 * MinuteInMs;
+export const name = 'uni';
 
 export function trade(candles: Candle[]) {
   return 'buy';
--- src/runner.ts
+++ src/runner.ts
@@ -1,1 +1,5 @@
-// every instrument, every candle run algo
+import { Candle } from './candle';
+
+export async function runAlgoOfACandle(candle: Candle, algoName: string, type: 'trade' | 'squareoff') {}
+
+export async function runAlgoForADay(candle: Candle, algoName: string) {}
