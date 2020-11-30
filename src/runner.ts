76f9affd9f849e8dacfcffd231d2dbd7b4b11200
Subhrashis Das <das.subhrashis@gmail.com>
2020-11-30 15:34:17 +0530

    Fixed imports

--- src/runner.ts
+++ src/runner.ts
@@ -1,5 +1,6 @@
-import { Candle } from './candle';
+import { Candle } from '@src/candle';
+import { Milliseconds } from '@src/date';
 
 export async function runAlgoOfACandle(candle: Candle, algoName: string, type: 'trade' | 'squareoff') {}
 
-export async function runAlgoForADay(candle: Candle, algoName: string) {}
+export async function runAlgoForADay(dateTime: Milliseconds, algoName: string) {}
