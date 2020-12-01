b86e34a54b718a485abfa1f4e67220910df7f6fe
Subhrashis Das <das.subhrashis@gmail.com>
2020-12-01 21:44:57 +0530

    Coding runner logic

--- src/runner.ts
+++ src/runner.ts
@@ -1,17 +1,21 @@
-import { Milliseconds } from '@src/date';
+import { ceilToNearestMilliseconds, DayInMs, Milliseconds } from '@src/date';
 import { filteredInstruments } from '@src/instrument';
 import { getOptimizedHistory } from '@src/history';
-import { Order } from '@src/order';
+import { Candle } from './candle';
 
 export async function runAlgo(from: Milliseconds, to: Milliseconds, algoName: string) {
   const instruments = await filteredInstruments(['2']);
   for (const instrument of instruments) {
     const candles = await getOptimizedHistory(from, to, instrument.id);
-    const ordersToBePlaced: Order[] = [];
+    let ordersToBePlaced: Candle[] = [];
     for (const candle of candles) {
-      // Run Algo
-      console.log(candle);
-      console.log(ordersToBePlaced);
+      // TODO: get data
+      // TODO: run algo
+      // TODO: get input and push.
+      // Repeat
+      if (candle.timestamp < ceilToNearestMilliseconds(candle.timestamp, DayInMs)) {
+        ordersToBePlaced = [];
+      }
     }
   }
 }
