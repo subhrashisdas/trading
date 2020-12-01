df131694e7afde7fa6a064c4ea0c913333d1f566
Subhrashis Das <das.subhrashis@gmail.com>
2020-12-01 16:03:11 +0530

    Added runner logic

--- src/runner.ts
+++ src/runner.ts
@@ -1,3 +1,17 @@
-import { Candle } from '@src/candle';
+import { Milliseconds } from '@src/date';
+import { filteredInstruments } from '@src/instrument';
+import { getOptimizedHistory } from '@src/history';
+import { Order } from '@src/order';
 
-export async function runAlgo(candles: Candle[], algoName: string) {}
+export async function runAlgo(from: Milliseconds, to: Milliseconds, algoName: string) {
+  const instruments = await filteredInstruments(['2']);
+  for (const instrument of instruments) {
+    const candles = await getOptimizedHistory(from, to, instrument.id);
+    const ordersToBePlaced: Order[] = [];
+    for (const candle of candles) {
+      // Run Algo
+      console.log(candle);
+      console.log(ordersToBePlaced);
+    }
+  }
+}
