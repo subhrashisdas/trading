8703cc6316611d1f95e8417801351d57e779848a
Subhrashis Das <das.subhrashis@gmail.com>
2020-12-08 19:48:32 +0530

    Refining logic

--- src/position.ts
+++ src/position.ts
@@ -1,4 +1,5 @@
 import { Milliseconds } from './date';
+import { functionCache } from '@src/fs';
 import { getCredentials } from '@src/token';
 import { jsonRequest } from '@src/request';
 
@@ -25,8 +26,8 @@ export async function getPositions(): Promise<Position[]> {
   return body?.data?.day as Position[];
 }
 
-export async function getQuantityByInstrumentId(instrumentId: number, cachedTill?: Milliseconds): Promise<number> {
-  const positions = await getPositions();
+export async function getQuantityByInstrumentId(instrumentId: number, cachedTill: Milliseconds): Promise<number> {
+  const positions = (await functionCache(getPositions, [], cachedTill)) as Position[];
   const position = positions.find((position) => position.instrument_token === instrumentId);
   return position ? position.quantity : 0;
 }
--- src/runner.ts
+++ src/runner.ts
@@ -1,26 +1,36 @@
 import { Candle } from '@src/candle';
 import { Instrument, filteredInstruments } from '@src/instrument';
-import { Milliseconds, MinuteInMs } from '@src/date';
+import { Milliseconds, MinuteInMs, ceilToNearestMilliseconds } from '@src/date';
 import { getAlgo } from '@src/algo';
 import { getOptimizedHistory } from '@src/history';
 import { getQuantityByInstrumentId } from '@src/position';
 
-export async function runAlgo(
-  from: Milliseconds,
-  to: Milliseconds,
-  algoName: string,
-  quantity: number,
-  isLive: Boolean
-) {
-  const instruments = await filteredInstruments(['2']);
+export interface RunAlgoOptions {
+  from: Milliseconds;
+  to: Milliseconds;
+  algoName: string;
+  quantity: number;
+  isLive: Boolean;
+  recurring: Milliseconds;
+  instrumentNames: string[];
+}
+
+export async function runAlgo(options: RunAlgoOptions) {
+  const instruments = await filteredInstruments(options.instrumentNames);
   for (const instrument of instruments) {
-    const history = await getOptimizedHistory(from, to, instrument.id);
+    const history = await getOptimizedHistory(options.from, options.to, instrument.id);
     for (const candle of history) {
-      await runAlgoEachCandle({
-        candle,
-        algoName,
-        instrument,
-      });
+      // Not correct logic, need refinement
+      let alreadyExecuted = false;
+      if (candle.timestamp < ceilToNearestMilliseconds(candle.timestamp, options.recurring) && !alreadyExecuted) {
+        alreadyExecuted = true;
+        await runAlgoEachCandle({
+          candle,
+          algoName: options.algoName,
+          instrument,
+          cacheTill: ceilToNearestMilliseconds(candle.timestamp, options.recurring),
+        });
+      }
     }
   }
 }
@@ -29,10 +39,11 @@ interface RunAlgoEachCandleOptions {
   candle: Candle;
   algoName: string;
   instrument: Instrument;
+  cacheTill: Milliseconds;
 }
 
 export async function runAlgoEachCandle(options: RunAlgoEachCandleOptions) {
-  const quantity = await getQuantityByInstrumentId(options.instrument.instrument_token);
+  const quantity = await getQuantityByInstrumentId(options.instrument.instrument_token, options.cacheTill);
   const algo = getAlgo(options.algoName);
   const algoFrom = options.candle.timestamp - algo.timeInterval;
   const algoTo = options.candle.timestamp + MinuteInMs;
