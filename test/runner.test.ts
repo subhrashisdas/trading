3c5b1c0c906a6d37f4e8d57cb4603be0ee7c337b
Subhrashis Das <das.subhrashis@gmail.com>
2021-01-19 11:26:46 +0530

    Added test

--- test/runner.test.ts
+++ test/runner.test.ts
@@ -1,7 +1,8 @@
 import { Instrument } from '@src/instrument';
+import { MinuteInMs, WeekInMs } from '@src/date';
 import { OhlvcCandle, convertOhlvcCandlesToTradeJson } from '@src/candle';
 import { deepStrictEqual } from 'assert';
-import { runAlgoEachCandle } from '@src/runner';
+import { runAlgo, runAlgoEachCandle } from '@src/runner';
 
 export async function runAlgoEachCandleTest() {
   const candle: OhlvcCandle[] = [['2020-11-12T15:15:00+0530', 12625.3, 12641.15, 12622.25, 12640.25, 0, 0]];
@@ -24,4 +25,16 @@ export async function runAlgoEachCandleTest() {
     price: 100,
   });
   deepStrictEqual(price, +100);
+}
+
+export async function runAlgoTest() {
+  await runAlgo({
+    from: Date.now() - 2 * WeekInMs,
+    to: Date.now() - WeekInMs,
+    algoName: 'test',
+    quantity: 1,
+    isLive: false,
+    recurring: 5 * MinuteInMs,
+    instrumentNames: ['5'],
+  });
 }
