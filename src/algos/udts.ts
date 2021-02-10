bb614e87407006299d08eae43bf214e48d37e14f
Subhrashis Das <das.subhrashis@gmail.com>
2021-02-10 23:55:43 +0530

    Missing weekly chart

--- src/algos/udts.ts
+++ src/algos/udts.ts
@@ -8,6 +8,7 @@ export const name = "udts";
 export const startAt = new Date(5 * HourInMs).getTime();
 export const endAt = new Date(10 * HourInMs).getTime();
 
+// TODO: I am missing 15 minutes chart
 export function trade(candles: Candle[]): number {
   const latestCandle = candles[candles.length - 1];
   const dailyTrend = candleChange(trendCandles(lodash.takeRight(convertInterval(candles, DayInMs), 4)));
