0bd364cdaa8b12080718c265d8577f525d037143
Subhrashis Das <das.subhrashis@gmail.com>
2021-02-10 14:58:26 +0530

    UDTS logic

--- src/algos/udts.ts
+++ src/algos/udts.ts
@@ -11,7 +11,7 @@ export function trade(candles: Candle[]): number {
   const latestCandle = candles[candles.length - 1];
   const dailyTrend = candleChange(trendCandles(convertInterval(candles, DayInMs)));
   const weeklyTrend = candleChange(trendCandles(convertInterval(candles, WeekInMs)));
-  const monthlyTrend = candleChange(trendCandles(convertInterval(candles, WeekInMs)));
+  const monthlyTrend = candleChange(trendCandles(convertInterval(candles, 4 * WeekInMs)));
   if (dailyTrend > 0 && weeklyTrend > 0 && monthlyTrend > 0) {
     return latestCandle.close;
   } else if (dailyTrend < 0 && weeklyTrend < 0 && monthlyTrend < 0) {
@@ -37,13 +37,13 @@ export function squareoff(price: number, candles: Candle[]): number {
   const latestCandle = candles[candles.length - 1];
   if (price > 0) {
     const percentUp = ((latestCandle.close - price) / price) * 100;
-    if (percentUp > 1 || percentUp < -1) {
+    if (percentUp > 3 || percentUp < -0.5) {
       return -latestCandle.close;
     }
     return 0;
   } else if (price < 0) {
-    const percentUp = ((latestCandle.close - Math.abs(price)) / price) * 100;
-    if (percentUp > 1 || percentUp < -1) {
+    const percentUp = ((latestCandle.close - Math.abs(price)) / Math.abs(price)) * 100;
+    if (percentUp > 3 || percentUp < -0.5) {
       return latestCandle.close;
     }
     return 0;
